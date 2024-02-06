51218810a1997a3d171e09bacd95e77a192ecbe7
GDPWinnerPranata <winner.pranata@gdplabs.id>
2024-02-06 16:02:05 +0700

    Squashed commit of the following:
    commit ad07fd2b3e815fcf8019b2a48f1a8b07a5f8e1e0
    Author: GDPWinnerPranata <winner.pranata@gdplabs.id>
    Date:   Tue Feb 6 16:01:48 2024 +0700
    
        s
    
    commit 8e3dbd7187ead2cdac98b7aac19670bf6644fda6
    Author: GDPWinnerPranata <winner.pranata@gdplabs.id>
    Date:   Tue Feb 6 13:53:11 2024 +0700
    
        s

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const crypto = require("crypto");
-const { EncryptCommand, KMSClient } = require("@aws-sdk/client-kms");
-const { SignJWT, importPKCS8 } = require("jose");
+const {
+  EncryptCommand,
+  KMSClient,
+  SignCommand,
+  GetPublicKeyCommand,
+} = require("@aws-sdk/client-kms");
 const fs = require("fs");
 const env = require("./env");
+const { importSPKI, jwtVerify } = require("jose");
 
 main().catch(console.error);
 
@@ -10,22 +15,45 @@ async function main() {
   const signerKeypair = await generateEncryptedKeyPair();
   const encryptKeypair = await generateEncryptedKeyPair();
 
-  const signerJwt = await sign(
-    signerKeypair.keypair,
-    signerKeypair.decryptedPrivateKey
-  );
-  const encryptJwt = await sign(
-    encryptKeypair.keypair,
-    signerKeypair.decryptedPrivateKey
-  );
+  console.log("========================================================");
+  console.log("|  Generated Keypair");
+  console.log("========================================================");
+  console.log(JSON.stringify({ signerKeypair, encryptKeypair }, null, 2));
+  console.log("\n\n");
+
+  const signerJwt = await sign(signerKeypair.keypair);
+  const encryptJwt = await sign(encryptKeypair.keypair);
+
+  console.log("========================================================");
+  console.log("|  Signed Keypair");
+  console.log("========================================================");
+  console.log(JSON.stringify({ signerJwt, encryptJwt }, null, 2));
+  console.log("\n\n");
 
   const filename = "keys.json";
   const data = JSON.stringify({ signerJwt, encryptJwt }, null, 2);
   fs.writeFileSync(filename, data);
 
-  console.log(data);
-  console.log();
+  const signerPublicKey = await getSignerPublicKey();
+
+  console.log("========================================================");
+  console.log("|  Signer Public Key");
+  console.log("========================================================");
+  console.log(signerPublicKey);
+  console.log("\n\n");
+
+  const verification = await jwtVerify(
+    signerJwt,
+    await importSPKI(signerPublicKey)
+  );
+  console.log("========================================================");
+  console.log("|  Verification Result");
+  console.log("========================================================");
+  console.log(JSON.stringify({ verification }, null, 2));
+  console.log("\n\n");
+  console.log("========================================================");
   console.log(`[!] Key generated at '${filename}' successfully!`);
+  console.log("========================================================");
 }
 
 // =================================== CORE ====================================
@@ -43,18 +71,53 @@ async function generateEncryptedKeyPair() {
   };
 }
 
-async function sign(payload, privateKey) {
-  const secret = await importPKCS8(privateKey);
-  const jwt = await new SignJWT(payload)
-    .setProtectedHeader({
-      alg: "ES256",
-    })
-    .sign(secret);
+async function sign(payload) {
+  const jwtHeader = {
+    alg: "ES256",
+  };
+  const signedString = `${Buffer.from(JSON.stringify(jwtHeader)).toString(
+    "base64url"
+  )}.${Buffer.from(JSON.stringify(payload)).toString("base64url")}`;
 
+  const kmsClient = getKmsClient();
+  const signCommand = new SignCommand({
+    Message: new Uint8Array(Buffer.from(signedString)),
+    KeyId: env.get("SIGN_KEY_ID"),
+    SigningAlgorithm: "ECDSA_SHA_256",
+  });
+
+  const generateOutput = await kmsClient.send(signCommand);
+  const signature = signatureToBase64(generateOutput.Signature);
+
+  const jwt = `${signedString}.${signature}`;
   return jwt;
 }
 
 // =================================== UTILS ===================================
+
+async function getSignerPublicKey() {
+  const client = getKmsClient();
+  const command = new GetPublicKeyCommand({
+    KeyId: env.get("SIGN_KEY_ID"),
+  });
+
+  const output = await client.send(command);
+  const publicKey = Buffer.from(output.PublicKey).toString("base64");
+  return `-----BEGIN PUBLIC KEY-----\n${publicKey
+    .match(/.{0,64}/g)
+    .join("\n")}-----END PUBLIC KEY-----`;
+}
+
+function signatureToBase64(bytes) {
+  const rLength = bytes[3];
+  const r = bytes.subarray(4, 4 + rLength);
+  const s = bytes.subarray(6 + rLength);
+
+  return Buffer.concat([
+    Buffer.from(r[0] === 0 ? r.subarray(1) : r),
+    Buffer.from(s[0] === 0 ? s.subarray(1) : s),
+  ]).toString("base64url");
+}
 
 function generateKeyPair() {
   const {
@@ -79,17 +142,11 @@ function generateKeyPair() {
 }
 
 async function encryptKMS(plaintext) {
-  const kmsClient = new KMSClient({
-    credentials: {
-      accessKeyId: env.get("ACCESS_KEY_ID"),
-      secretAccessKey: env.get("SECRET_ACCESS_KEY"),
-    },
-    region: env.get("REGION"),
-  });
+  const kmsClient = getKmsClient();
 
   const encryptCommand = new EncryptCommand({
     Plaintext: new Uint8Array(Buffer.from(plaintext)),
-    KeyId: env.get("KEY_ID"),
+    KeyId: env.get("ENCRYPT_KEY_ID"),
   });
 
   const encryptionResult = await kmsClient.send(encryptCommand);
@@ -97,4 +154,14 @@ async function encryptKMS(plaintext) {
     "base64"
   );
   return ciphertext;
+}
+
+function getKmsClient() {
+  return new KMSClient({
+    credentials: {
+      accessKeyId: env.get("ACCESS_KEY_ID"),
+      secretAccessKey: env.get("SECRET_ACCESS_KEY"),
+    },
+    region: env.get("REGION"),
+  });
 }
